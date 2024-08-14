f52cf11665e8cd3099a49c28745c9ca4bbb6dfe3
Alex Batisse <alex.batisse@canonical.com>
2024-08-14 14:37:54 +0200

    Remove uneeded zellij theme

--- home/shell/zellij/default.nix
+++ home/shell/zellij/default.nix
@@ -5,7 +5,6 @@
 }: {
   home.file = {
     ".config/zellij/config.kdl".source = ./config.kdl;
-    ".config/zellij/themes/catpuccin.kdl".source = ./themes/catppuccin.kdl;
   };
 
   programs.zellij.enable = true;
